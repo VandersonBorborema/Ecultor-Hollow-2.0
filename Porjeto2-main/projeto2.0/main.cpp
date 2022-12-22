a4a80ef39cde895d0947bc6280a3402776fbff02
VandersonBorborema <vanderson.borborema03@gmail.com>
2022-12-22 09:55:41 -0300

    Novo

--- Porjeto2-main/projeto2.0/main.cpp
+++ Porjeto2-main/projeto2.0/main.cpp
@@ -8,6 +8,25 @@
 #include <vector>
 
 int main(){
+  Sculptor *teste;
+  Interpretador parser;
+
+  std::vector<FiguraGeometrica*> fig;
+
+  fig = parser.parse((char *)"C:/Users/elvan/OneDrive/Área de Trabalho/Projeto#U/Dados/figuras.txt");
+  teste = new Sculptor(parser.getDimx(), parser.getDimy(), parser.getDimz());
+
+  for(size_t i=0; i<fig.size(); i++){
+    std::cout << "draw\n";
+    fig[i]->draw(*teste);
+  }
+  teste->Sculptor::writeOFF((char *)"C:/Users/elvan/OneDrive/Área de Trabalho/Projeto#U/Dados/teste.off");
+  for(size_t i=0; i<fig.size(); i++){
+    delete fig[i];
+    }
+  delete teste;
+
+
   Sculptor *hollow;
   Interpretador reader;
 
